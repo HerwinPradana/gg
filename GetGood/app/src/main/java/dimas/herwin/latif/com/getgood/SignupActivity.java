76f95a3d188af2b60ccef6bae165de6453b1ebb1
yanoo <dimasyanu@yahoo.co.id>
2016-12-08 15:16:08 +0700

    update login view

--- GetGood/app/src/main/java/dimas/herwin/latif/com/getgood/SignupActivity.java
+++ GetGood/app/src/main/java/dimas/herwin/latif/com/getgood/SignupActivity.java
@@ -1,5 +1,6 @@
 package dimas.herwin.latif.com.getgood;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,6 +9,7 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +44,19 @@ public class SignupActivity extends AppCompatActivity{
         textViewErrors   = (TextView) findViewById(R.id.errors);
 
         sharedPreferences = getSharedPreferences(getString(R.string.app_pref), MODE_PRIVATE);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void signup(View view){
